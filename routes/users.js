f70a0de10a0a9a3c7b8a51956b0870d0ebffb072
Juan Parra <juancasomoza@hotmail.com>
2024-10-21 20:11:47 -0500

    CRUD users OK

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
-import { register, testUser, login } from "../controllers/user.js";
+import { register, testUser, login, profile, listUsers, updateUser } from "../controllers/user.js";
+import { ensureAuth} from '../middlewares/auth.js';
 
 const router = Router();
 
 // Definir rutas de user
-router.get('/test-user', testUser);
+router.get('/test-user', ensureAuth, testUser);
 router.post('/register', register);
-router.post('/login', login)
+router.post('/login', login);
+router.get('/profile/:id', ensureAuth, profile);
+router.get('/list/:page?', ensureAuth, listUsers);
+router.put('/update', ensureAuth, updateUser);
 
 // Export el Router
 export default router;
